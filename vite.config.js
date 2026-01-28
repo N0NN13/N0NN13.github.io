ad8f5263beb2432c5b19c5808b23fff0bd840a4b
N0NN13 <ethan.blair300@gmail.com>
2026-01-28 16:18:29 -0700

    Fix base path for root domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Vital for relative paths on GitHub Pages
+  base: '/', // Ensure absolute path for root domain deployment
 })
